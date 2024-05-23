e4a6db70bc1d5c7d2152f4639e19cb91da1dbfbe
harisawan <harisawan03@gmail.com>
2024-05-23 16:13:28 -0500

    Add project files.

---
+++ TcHmiJoystick/Joystick/Joystick.ts
@@ -0,0 +1,104 @@
+/*
+ * Generated 5/23/2024 4:07:55 PM
+ * Copyright (C) 2024
+ */
+module TcHmi {
+    export module Controls {
+        export module TcHmiJoystick {
+            export class Joystick extends TcHmi.Controls.System.TcHmiControl {
+
+                /*
+                Attribute philosophy
+                --------------------
+                - Local variables are not set in the class definition, so they have the value 'undefined'.
+                - During compilation, the Framework sets the value that is specified in the HTML or in the theme (possibly 'null') via normal setters.
+                - Because of the "changed detection" in the setter, the value is only processed once during compilation.
+                - Attention: If we have a Server Binding on an Attribute, the setter will be called once with null to initialize and later with the correct value.
+                */
+
+                /**
+                 * Constructor of the control
+                 * @param {JQuery} element Element from HTML (internal, do not use)
+                 * @param {JQuery} pcElement precompiled Element (internal, do not use)
+                 * @param {TcHmi.Controls.ControlAttributeList} attrs Attributes defined in HTML in a special format (internal, do not use)
+                 * @returns {void}
+                 */
+                constructor(element: JQuery, pcElement: JQuery, attrs: TcHmi.Controls.ControlAttributeList) {
+                    /** Call base class constructor */
+                    super(element, pcElement, attrs);
+                }
+
+                protected __elementTemplateRoot!: JQuery;
+
+                /**
+                 * Raised after the control was added to the control cache and the constructors of all base classes were called.
+                 */
+                public __previnit() {
+                    // Fetch template root element
+                    this.__elementTemplateRoot = this.__element.find('.TcHmi_Controls_TcHmiJoystick_Joystick-Template');
+                    if (this.__elementTemplateRoot.length === 0) {
+                        throw new Error('Invalid Template.html');
+                    }
+                    // Call __previnit of base class
+                    super.__previnit();
+                }
+                /**
+                 * Is called during control initialization after the attribute setters have been called. 
+                 * @returns {void}
+                 */
+                public __init() {
+                    super.__init();
+                }
+
+                /**
+                 * Is called by the system after the control instance is inserted into the active DOM.
+                 * Is only allowed to be called from the framework itself!
+                 */
+                public __attach() {
+                    super.__attach();
+
+                    /**
+                     * Initialize everything which is only available while the control is part of the active dom.
+                     */
+                }
+
+                /**
+                 * Is called by the system when the control instance is no longer part of the active DOM.
+                 * Is only allowed to be called from the framework itself!
+                 */
+                public __detach() {
+                    super.__detach();
+
+                    /**
+                     * Disable everything that is not needed while the control is not part of the active DOM.
+                     * For example, there is usually no need to listen for events!
+                     */
+                }
+
+                /**
+                 * Destroy the current control instance. 
+                 * Will be called automatically if system destroys control!
+                 */
+                public destroy() {
+                    /**
+                     * Ignore while __keepAlive is set to true.
+                     */
+                    if (this.__keepAlive) {
+                        return;
+                    }
+
+                    super.destroy();
+
+                    /**
+                     * Free resources like child controls etc.
+                     */
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Register Control
+ */
+TcHmi.Controls.registerEx('Joystick', 'TcHmi.Controls.TcHmiJoystick', TcHmi.Controls.TcHmiJoystick.Joystick);
